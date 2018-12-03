16d608c5eb28f34cfbd8b229e40ff0f21ba0289b
Artur Kaleta <artur970808@gmail.com>
2018-12-03 22:07:36 +0100

    fix while loop

--- src/neighbornhood.py
+++ src/neighbornhood.py
@@ -8,13 +8,12 @@ def first_solution(test_case: dict[str, tuple], t_displacement: List[List[int]],
     ps = 0  # punkty satysfakcji
     last_i = 0  # nr. miejsca odwiedzonego w poprzedniej iteracji
     ans = {}  # rozwiazanie
-    visited = {}  # slownik miejsc juz odwiedzonych {nr. miejsca[int]: czy byl odwiedzony[bool]}
+    visited = {}  # miejsca juz odwiedzone {nr. miejsca[int]: czy byl odwiedzony[bool]}
+    rand_but_not_visited = set()  # miejsca wylosowane ale nie spełniające warunków
 
     get_key = lambda index: list(test_case.keys())[index]  # zwraca klucz
 
-    while available_time > ti:
-        # TODO: mechanizm kontrolujacy ilosc iteracji w momencie gdy pozostały czas jest mniejszy od od minimalnego
-        # TODO:(petla nieskonczona)
+    while available_time > ti and len(rand_but_not_visited) < len(test_case):
         i = randint(0, len(test_case))  # numer elementu rozwiązania
 
         if last_i != 0:
@@ -23,22 +22,27 @@ def first_solution(test_case: dict[str, tuple], t_displacement: List[List[int]],
         # test_case: dict[get_key(i)][x, y, t_otwarcia, t_zamkniecia, ps, t_maxPS]
 
         if i in visited and test_case[get_key(i)][2] < ti < test_case[get_key(i)][4]:
-            # sprawdzenie czy element i nie był jeszcze odwiedzony oraz sprawdzenie funkcji dopuszczalnych
+            # sprawdzenie funkcji dopuszczalnych
 
             alpha = random(0, 1)
 
             if ti + test_case[get_key(i)][5] * alpha < available_time:
+                # sprawdzenie czy dane miejse zmiescimy w dostepnym czasie
+
                 ans[get_key(i)] = alpha
                 ps += int(alpha * test_case[get_key(i)][4])
 
                 visited[i] = True
                 last_i = i
+                rand_but_not_visited.clear()  # znalezlismy rozwiazanie, czyscimy zbior
+
+        else:
+            rand_but_not_visited.add(i)  # dodajemy rozwiazanie do zbioru
 
     return [ans, ps]
 
 
 # TODO: funkcja next_solution(solution)
-
 
 def next_solution(test_case: dict[str, tuple], t_diplacement: List[List[int]],
                   solution: dict[str, float]) -> List[dict[str: float], int]:
