591a576273724bebbf4ad35343e9c490e826812e
Artur Kaleta <artur970808@gmail.com>
2018-12-03 21:21:57 +0100

    little corrections

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,26 +1,29 @@
 from math import exp
 from random import random
 from typing import List
-from src.neighbornhood import first_solution
+from src.neighbornhood import first_solution, next_solution
 
 
-def algorithm(test_case: dict[str, tuple[6]], t_displacement: List[List[int]],
-                   available_time: int, t_max: int, t_min: int, len_of_sol: int) -> List[dict[List[str]: List[float]]]:
+def algorithm(test_case: dict[str, tuple], t_displacement: List[List[int]],
+              available_time: int, t_max: int, t_min: int, len_of_sol: int) -> List[dict[List[str]: List[float]]]:
 
-    [solution, e_c] = first_solution(test_case, t_displacement, available_time)  # pierwsze rozwiązanie i jego punkty
-    solutions = List[solution]                          # lista dobrych rozwiązań
+    [solution, energy_first] = first_solution(test_case, t_displacement, available_time)  # pierwsze rozwiązanie i
+    # jego punkty
+    solutions = []  # lista dobrych rozwiązań
 
-    for t in  range(t_max, t_min):
-        [next_solution, e_n] = next_solution(solution)  # następne rozwiązanie i jego punkty
-        delta_e = e_n - e_c                             # różnica w punktach pomiędzy rozwiązaniami
+    for t in range(t_max, t_min):
+        [next_sol, energy_next] = next_solution(solution)  # następne rozwiązanie i jego punkty
+        delta_e = energy_next - energy_first  # różnica w punktach pomiędzy rozwiązaniami
 
         if delta_e > 0:
-            solution = next_solution
+            solution = next_sol
+            energy_first = energy_next
             solutions.append(solution)
-        elif exp(delta_e/t) > random(0,1):
-            solution = next_solution
+        elif exp(delta_e / t) > random(0, 1):
+            energy_first = energy_next
+            solution = next_sol
 
-        if len(solutions) > len_of_sol:                # sprawdź czy lista nie jest przepełniona
+        if len(solutions) > len_of_sol:  # sprawdź czy lista nie jest przepełniona
             del solutions[0]
 
     return solutions
--- src/neighbornhood.py
+++ src/neighbornhood.py
@@ -1,30 +1,45 @@
 from random import randint, random
 from typing import List
 
-# TODO: funkcja next_solution(solution)
 
-def first_solution(test_case: dict[str, tuple[6]], t_displacement: List[List[int]],
-                   available_time: int) -> dict[List[str]: List[float]]:  # -> dict['nazwa': alpha, PS]
+def first_solution(test_case: dict[str, tuple], t_displacement: List[List[int]],
+                   available_time: int) -> List[dict[str: float], int]:  # -> List[dict['nazwa': alpha], PS]
     ti = 0  # aktualny czas
     ps = 0  # punkty satysfakcji
-    last_i = 0
-    ans: dict[List[str]: List[float]]
-    get_key = lambda index: list(test_case.keys())[index] # zwraca klucz
+    last_i = 0  # nr. miejsca odwiedzonego w poprzedniej iteracji
+    ans = {}  # rozwiazanie
+    visited = {}  # slownik miejsc juz odwiedzonych {nr. miejsca[int]: czy byl odwiedzony[bool]}
 
-    while ti < available_time:
+    get_key = lambda index: list(test_case.keys())[index]  # zwraca klucz
 
+    while available_time > ti:
+        # TODO: mechanizm kontrolujacy ilosc iteracji w momencie gdy pozostały czas jest mniejszy od od minimalnego
+        # TODO:(petla nieskonczona)
         i = randint(0, len(test_case))  # numer elementu rozwiązania
-        # TODO: dodać mechanizm sprawdzający czy dane miejsce nie zostało już odwiedzone
+
         if last_i != 0:
             ti += t_displacement[last_i][i]
 
-        # dict[get_key(i)][x, y, t_otwarcia, t_zamkniecia, ps, t_maxPS]
+        # test_case: dict[get_key(i)][x, y, t_otwarcia, t_zamkniecia, ps, t_maxPS]
 
-        if test_case[get_key(i)][2] < ti < test_case[get_key(i)][4]:
+        if i in visited and test_case[get_key(i)][2] < ti < test_case[get_key(i)][4]:
+            # sprawdzenie czy element i nie był jeszcze odwiedzony oraz sprawdzenie funkcji dopuszczalnych
+
             alpha = random(0, 1)
 
             if ti + test_case[get_key(i)][5] * alpha < available_time:
                 ans[get_key(i)] = alpha
-                ps += int(alpha*test_case[get_key(i)][4])
+                ps += int(alpha * test_case[get_key(i)][4])
+
+                visited[i] = True
+                last_i = i
 
     return [ans, ps]
+
+
+# TODO: funkcja next_solution(solution)
+
+
+def next_solution(test_case: dict[str, tuple], t_diplacement: List[List[int]],
+                  solution: dict[str, float]) -> List[dict[str: float], int]:
+    return 0
