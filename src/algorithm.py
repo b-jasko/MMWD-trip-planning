6932dd9b2b6db273c37936da1e6ed2f4aedb330c
Artur Kaleta <artur970808@gmail.com>
2018-12-05 22:02:03 +0100

    add subtract file, some corrections

--- src/algorithm.py
+++ src/algorithm.py
@@ -4,23 +4,26 @@ from typing import List
 from src.neighbornhood import first_solution, next_solution
 
 
-def algorithm(test_case: dict[str, tuple], t_displacement: List[List[int]],
-              available_time: int, t_max: int, t_min: int, len_of_sol: int) -> List[dict[List[str]: List[float]]]:
+def algorithm(test_case: dict, t_displacement: List[List[int]],
+              available_time: int, t_max: int, t_min: int, len_of_sol: int) -> List[List[str], List[float], int]:
 
-    [solution, energy_first] = first_solution(test_case, t_displacement, available_time)  # pierwsze rozwiązanie i
+    [solution, alphas, energy] = first_solution(test_case, t_displacement, available_time)  # pierwsze rozwiązanie i
     # jego punkty
     solutions = []  # lista dobrych rozwiązań
 
     for t in range(t_max, t_min):
-        [next_sol, energy_next] = next_solution(solution)  # następne rozwiązanie i jego punkty
-        delta_e = energy_next - energy_first  # różnica w punktach pomiędzy rozwiązaniami
+        [next_sol, alphas_next, energy_next] = next_solution(test_case, t_displacement, solution, alphas, energy,
+                                                             available_time)  # następne rozwiązanie i jego punkty
+        delta_e = energy_next - energy  # różnica w punktach pomiędzy rozwiązaniami
 
         if delta_e > 0:
             solution = next_sol
-            energy_first = energy_next
-            solutions.append(solution)
+            alphas = alphas_next
+            energy = energy_next
+            solutions.append(solution, alphas, energy)
         elif exp(delta_e / t) > random(0, 1):
-            energy_first = energy_next
+            energy = energy_next
+            alphas = alphas_next
             solution = next_sol
 
         if len(solutions) > len_of_sol:  # sprawdź czy lista nie jest przepełniona
--- src/neighbornhood.py
+++ src/neighbornhood.py
@@ -1,13 +1,16 @@
 from random import randint, random
 from typing import List
 
+# TODO: Zamienic typ t_displacement na zbiór set
 
-def first_solution(test_case: dict[str, tuple], t_displacement: List[List[int]],
-                   available_time: int) -> List[dict[str: float], int]:  # -> List[dict['nazwa': alpha], PS]
+
+def first_solution(test_case: dict, t_displacement: List[List[int]],
+                   available_time: int) -> [List[str], List[float], int]:  # -> List[dict['nazwa': alpha], PS]
     ti = 0  # aktualny czas
     ps = 0  # punkty satysfakcji
     last_i = 0  # nr. miejsca odwiedzonego w poprzedniej iteracji
-    ans = {}  # rozwiazanie
+    ans = []  # rozwiazanie
+    alphas = []  # współczynniki alfa
     visited = {}  # miejsca juz odwiedzone {nr. miejsca[int]: czy byl odwiedzony[bool]}
     rand_but_not_visited = set()  # miejsca wylosowane ale nie spełniające warunków
 
@@ -21,7 +24,7 @@ def first_solution(test_case: dict[str, tuple], t_displacement: List[List[int]],
 
         # test_case: dict[get_key(i)][x, y, t_otwarcia, t_zamkniecia, ps, t_maxPS]
 
-        if i in visited and test_case[get_key(i)][2] < ti < test_case[get_key(i)][4]:
+        if i not in visited and test_case[get_key(i)][2] < ti < test_case[get_key(i)][3]:
             # sprawdzenie funkcji dopuszczalnych
 
             alpha = random(0, 1)
@@ -29,7 +32,8 @@ def first_solution(test_case: dict[str, tuple], t_displacement: List[List[int]],
             if ti + test_case[get_key(i)][5] * alpha < available_time:
                 # sprawdzenie czy dane miejse zmiescimy w dostepnym czasie
 
-                ans[get_key(i)] = alpha
+                ans[i] = get_key(i)
+                alphas[i] = alpha
                 ps += int(alpha * test_case[get_key(i)][4])
 
                 visited[i] = True
@@ -39,11 +43,58 @@ def first_solution(test_case: dict[str, tuple], t_displacement: List[List[int]],
         else:
             rand_but_not_visited.add(i)  # dodajemy rozwiazanie do zbioru
 
-    return [ans, ps]
+    return [ans, alphas, ps]
 
 
-# TODO: funkcja next_solution(solution)
+# TODO: funkcja next_solution(solution): dodawanie nowego elementu do rozwiązania
 
-def next_solution(test_case: dict[str, tuple], t_diplacement: List[List[int]],
-                  solution: dict[str, float]) -> List[dict[str: float], int]:
-    return 0
+def next_solution(test_case: dict, t_displacement: dict[str:dict[str:int]], solution: List[str], alphas: List[float],
+                  ps: int, available_time: int) -> List[dict[str: float], int]:
+
+    rand_but_not_visited = set()
+
+    first_elem = random.choice(solution)  # wylosowanie elementu rozwiązania
+    [ti, available_time] = subtract_first_elem_from_solution(solution, alphas, t_displacement,
+                                                             test_case, first_elem, available_time)
+
+    while len(rand_but_not_visited) < len(test_case):
+        second_elem = random.chice(test_case.keys())  # wylosowanie elementu dostępnych rozwiązań
+
+        if second_elem not in solution and test_case[second_elem][2] < ti < test_case[second_elem][3]:
+            alpha = random(0, 1)
+
+            if available_time > alpha*test_case[second_elem][5]:
+                ########
+
+                ########
+
+                rand_but_not_visited.clear()
+        else:
+            rand_but_not_visited = second_elem
+
+    return [solution, alphas, ps]
+
+
+def subtract_first_elem_from_solution(solution: List[str], alphas: List[float], t_displacement: dict[str:dict[str:int]],
+                                      test_case: dict, first_elem: str, available_time: int) -> [int, int]:
+
+    used_time = 0  # wykorzystany czas
+    ti = 0  # aktualny czas
+
+    for elem, alpha in solution, alphas:
+        if elem == first_elem:
+            if last_elem:
+                used_time -= t_displacement[last_elem][elem]
+                last_elem = elem
+                ti = used_time
+        elif last_elem == first_elem:
+            used_time -= t_displacement[last_elem][elem]
+            used_time += test_case[elem][5] * alpha
+            last_elem = elem
+        else:
+            used_time += test_case[elem][5] * alpha
+            last_elem = elem
+
+        available_time -= used_time
+
+    return [ti, available_time]
