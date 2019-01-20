66564aadc720c0d5a89652309df1605071dc344f
b-jasko <bjasko97@gmail.com>
2019-01-20 17:51:03 +0100

    add named arguments in call of algorithm fuction

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     test_case = import_testcase()
     plot = OutputData([], [])
     start = time.time()
-    listen = algorithm(test_case, 1, 130, 1500, 1, 1, 10, plot)
+    listen = algorithm(test_case, velocity=1, available_time=130, t_max=1500, t_min=1, num_of_neig=1, len_of_sol=10, out_data=plot)
     for i in listen:
         print(i.answer)
         print(i.satisfaction_points)
